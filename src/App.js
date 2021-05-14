bddc020ee3573e57ca49f9939e8793424eb69b82
Edward Kabue <ekabue0@gmail.com>
2021-05-14 15:56:50 +0300

    added collectoion previews

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
+import { Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </switch>
     </div>
   );
 }
