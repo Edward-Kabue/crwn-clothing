b98196c8826617003de1c77a28432d54d4e59d3a
Edward Kabue <ekabue0@gmail.com>
2021-05-14 13:14:21 +0300

    First commit

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from "../../components/directory/directory.componets";
+
+const HomePage = () => (
+  <div className="homapage">
+    {" "}
+    <Directory />
+  </div>
+);
+
+export default HomePage;
