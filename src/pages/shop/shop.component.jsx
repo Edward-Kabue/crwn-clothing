bda343d83e80f45572e893755f013a117551a94f
Edward Kabue <ekabue0@gmail.com>
2021-05-15 09:09:57 +0300

    updating our app with a shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CollectionPreview from "../../components/preview-collection/collection-preview";
+import CollectionPreview from "../../components/collection-preview/collection-preview";
 
 import SHOP_DATA from "./shop.data";
 class ShopPage extends React.Component {
