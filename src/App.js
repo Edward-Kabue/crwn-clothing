84dc09be8d2875b0b8ee4688c4aa68ff370ca1b6
Edward Kabue <ekabue0@gmail.com>
2021-05-25 05:18:43 +0300

    Added link to signin component and custom signin button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route } from "react-router-dom";
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-
+import SignInAndSignup from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 function App() {
   return (
     <div>
@@ -12,6 +12,7 @@ function App() {
       <switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignup} />
       </switch>
     </div>
   );
