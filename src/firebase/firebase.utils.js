ec4ea5219fea76e8839005b914f36bc833696d6c
Edward Kabue <ekabue0@gmail.com>
2021-06-06 14:09:16 +0300

    Storing sign in with email and password in the firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,15 +11,38 @@ const config = {
   appId: "1:416894856802:web:94532b765413054999ab3b",
   measurementId: "G-PTMGNJ977J",
 };
-
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
-
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
